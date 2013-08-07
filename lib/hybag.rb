8c209661e389c6c7ba159768e38db34c7de61898
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-07 13:45:03 -0700

    Changed requires.

--- lib/hybag.rb
+++ lib/hybag.rb
@@ -2,12 +2,9 @@ require 'active_fedora'
 require 'bagit'
 require 'mime-types'
 
+require 'hybag/version'
+require 'hybag/baggable'
+require 'hybag/validator'
+require 'hybag/bag_writer'
 module Hybag
-  extend ActiveSupport::Autoload
-
-  require :Version
-  require :Baggable
-  require :Validator
-  require :BagWriter
-
 end
