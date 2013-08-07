8ac8ecd581074387751058ebf7faa25371580630
Trey Terrell <trey.terrell@oregonstate.edu>
2013-08-07 13:37:35 -0700

    Changed autoload to require.

--- lib/hybag.rb
+++ lib/hybag.rb
@@ -5,9 +5,9 @@ require 'mime-types'
 module Hybag
   extend ActiveSupport::Autoload
 
-  autoload :Version
-  autoload :Baggable
-  autoload :Validator
-  autoload :BagWriter
+  require :Version
+  require :Baggable
+  require :Validator
+  require :BagWriter
 
 end
